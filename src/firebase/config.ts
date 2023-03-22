908b90b99fac499c039fb36276c773bac6380c73
Nguy Minh Tai [Tai.Nguy] <Tai.Nguy@nexondv.com>
2023-03-22 18:27:02 +0700

    feat: add assets

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyARriNE0DvCaHOHPYyYgnfTxKNUJsukKgI',
-  authDomain: 'note-app-fullstack.firebaseapp.com',
-  projectId: 'note-app-fullstack',
-  storageBucket: 'note-app-fullstack.appspot.com',
-  messagingSenderId: '871809995609',
-  appId: '1:871809995609:web:a681669ba3934428f8b248',
-  measurementId: 'G-CKD68W5NBE'
+  apiKey: 'AIzaSyCnFaduV5KEi0jGzHHxd0D17jtwtnA_v3s',
+  authDomain: 'one-drink-and-food-369215.firebaseapp.com',
+  projectId: 'one-drink-and-food-369215',
+  storageBucket: 'one-drink-and-food-369215.appspot.com',
+  messagingSenderId: '913781430893',
+  appId: '1:913781430893:web:eea81cdcb2f1eaf3393d64',
+  measurementId: 'G-5F227V4NY6'
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+export { db, storage }
