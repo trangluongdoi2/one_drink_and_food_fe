3e4fd01d93e68ba879808c645ba77640d3008c0d
Vinh Nguyen <vinhnguyentan@rowboatsoftware.com>
2023-11-06 00:37:25 +0700

    feat: add create category product modal

--- src/configs/urlConfig.ts
+++ src/configs/urlConfig.ts
@@ -2,8 +2,6 @@ export const DOMAIN_SERVER = 'http://18.141.230.66:3051'
 
 export const API_URL = import.meta.env.PROD ? DOMAIN_SERVER : (import.meta.env.VITE_REACT_APP_API_URL as string)
 
-// export const API_URL = DOMAIN_SERVER
-
 export const ADMIN_LOGIN_URL = `${API_URL}/auth/admins/login`
 
 export const ADMIN_LOGOUT_URL = `${API_URL}/auth/admins/logout`
@@ -17,8 +15,6 @@ export const PRODUCT_DETAIL = `${API_URL}/products/detail/:type/:id`
 export const PRODUCT_CREATE_URL = `${API_URL}/products`
 
 export const CATEGORY_CREATE_URL = `${API_URL}/categories`
-
-// export const CATEGORY_FIND_BY_PRODUCT_TYPE = `${API_URL}/categories`
 
 export const CATEGORY_FIND_ALL_URL = `${API_URL}/categories`
 
--- src/features/uploadImages/index.ts
+++
@@ -1,3 +0,0 @@
-export default function uploadImages() {
-  console.log('uploadImages...')
-}
